d11a97bcae2523df1914f7667ce27f3cdea62894
Shiva5314 <9018539@student.zadine.nl>
2022-12-07 14:30:28 +0100

    ss

---
+++ opdracht3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$x = "Shiva Wamsteeker";
+$y = 'Klas=1G';
+
+echo $x;
+echo "<br>"; 
+echo $y;
+echo "<br>";
+$naam = "Shiva Wamsteeker !";
+
+echo "$naam";
+echo "<br>";
+$denk = "numberssss";
+
+echo $denk;
+echo "<br>";
+?>
+
+</body>
+</html>
